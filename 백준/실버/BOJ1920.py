222b44b189ebad52e72f0421e8572f97ed8f6a7b
김서영 <108572392+ssyng162@users.noreply.github.com>
2025-09-22 15:19:20 +0900

    2509.22

--- 백준/실버/BOJ1920.py
+++ 백준/실버/BOJ1920.py
@@ -1,5 +1,6 @@
 ### 백준 1920: 수 찾기
 
+#이분탐색
 import sys
 input = sys.stdin.readline
 
@@ -25,3 +26,19 @@ for num in nums:
         elif A[mid] > num:
             end = mid - 1
     print(1 if is_exist else 0)
+
+#set 사용
+#set으로 풀기
+
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+A = list(map(int, input().split()))
+M = int(input())
+nums = list(map(int, input().split()))
+
+A = set(A)
+
+for num in nums:
+    print(1 if num in A else 0)
