241a55161f41769a596888d37625fed2da2b6c8e
김서영 <108572392+ssyng162@users.noreply.github.com>
2025-09-22 09:51:58 +0900

    25.09.22

---
+++ 백준/골드/BOJ2293.py
@@ -0,0 +1,16 @@
+### 백준 2293: 동전 1
+
+import sys
+input = sys.stdin.readline
+
+n, k = map(int, input().split())
+values = [int(input()) for _ in range(n)]
+
+dp = [0] * (k+1)
+dp[0] = 1
+
+for value in values:
+    for i in range(value, k+1):
+        dp[i] += dp[i - value]
+        
+print(dp[k])
