95e5a7a52aad79158fb6e20d202fe0bdcab9f74e
김서영 <108572392+ssyng162@users.noreply.github.com>
2026-03-26 13:03:56 +0900

    2026.03.26

---
+++ 백준/실버/BOJ11286.py
@@ -0,0 +1,19 @@
+# 백준 11286: 절댓값 힙
+
+import heapq
+import sys
+
+input = sys.stdin.readline
+heap = []
+
+N = int(input())
+
+for _ in range(N):
+    x = int(input())
+    if x == 0:
+        if heap:
+            print(heapq.heappop(heap)[1])
+        else:
+            print(0)
+    else:
+        heapq.heappush(heap, (abs(x), x))
