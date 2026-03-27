305612b43089ff9f4a038673835da198940e1283
김서영 <108572392+ssyng162@users.noreply.github.com>
2026-03-27 19:10:44 +0900

    26.03.27

---
+++ 프로그래머스/Lv2/전화번호목록.py
@@ -0,0 +1,7 @@
+# 프로그래머스: 전화번호 목록
+
+def solution(phone_book):
+    phone_book.sort()
+    for i in range(1, len(phone_book)):
+        if phone_book[i].startswith(phone_book[i-1]): return False
+    return True
